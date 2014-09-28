2ee4cc3ef2e1cbad6ded00544b945cbd001fea63
arhaasdev <arhaasdev@users.noreply.github.com>
2014-09-27 22:17:09 -0700

    Create infrastructure-001.cpp

---
+++ infrastructure-001.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : infrastructure-001.cpp
+// Author      : Adam Robert Haas
+// Version     :
+// Copyright   : Marmot willpower is considerable.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "infrastructure!" << std::endl; // infrastructure!
+	return 0;
+}
+
