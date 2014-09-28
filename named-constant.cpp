dc2fdfdb88ee7f1c17db83aae21275e9b22ebc3a
arhaasdev <arhaasdev@users.noreply.github.com>
2014-09-27 22:28:15 -0700

    Create named-constant.cpp

---
+++ named-constant.cpp
@@ -0,0 +1,26 @@
+//============================================================================
+// Name        : named-constant.cpp
+// Author      : Adam Robert Haas
+// Version     :
+// Copyright   : Marmot willpower is considerable.
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	const double RATE = 6.9;
+	double deposit;
+
+	cout << "Enter the amount of your deposit $";
+	cin >> deposit;
+
+	double newBalance;
+	newBalance = deposit + deposit * (RATE / 100);
+	cout << "In one year, that deposit will grow to\n"
+		 << "$" << newBalance << " an amount worth waiting for.\n";
+
+	return 0;
+}
